7109bdb75d953b17d45257dea783ff7aebc20073
taismichely <michely2207@gmail.com>
2025-07-10 14:59:27 -0300

    fix: Formatação em importação

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,10 +1,5 @@
 import { IsNotEmpty } from 'class-validator';
-import {
-    Column,
-    Entity,
-    ManyToOne,
-    PrimaryGeneratedColumn
-} from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Categoria } from '../../categoria/entities/categoria.entity';
 import { Usuario } from './../../usuario/entities/usuario.entity';
 
